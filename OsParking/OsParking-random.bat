8cf9052b571101dd0e6c9e2cb408bae457ce93dc
Another Park <jbpark103@hanmail.net>
2016-10-26 08:42:17 +0900

    2016-Oct-26, OsParking Function Test Ready Version 2.1.1
    Burned from the source commit ID 55295be8
    ID in full : 55295be8765ba119d567217b7b844ddb2080d90d

--- OsParking/OsParking-random.bat
+++ OsParking/OsParking-random.bat
@@ -1,2 +1,2 @@
-start /high /B javaw -Xmx260m -Xms120m -Xmn180m -XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=2  -Dsun.java2d.d3d=false -jar OSParking-2.1.0-jar-with-dependencies.jar -debug -version:release -random 
+start /high /B javaw -Xmx260m -Xms120m -Xmn180m -XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=2  -Dsun.java2d.d3d=false -jar OSParking-2.1.1-jar-with-dependencies.jar -debug -version:release -random 
 exit
